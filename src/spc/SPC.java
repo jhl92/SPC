ef7b9093ac5ae6f72604c25e1fa337daf87c39a8
jhl92 <thomasnub@hotmail.com>
2014-03-19 10:52:47 +0100

    hej alle

---
+++ src/spc/SPC.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package spc;
+
+/**
+ *
+ * @author Junheng Li
+ */
+public class SPC
+{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+        // TODO code application logic here
+    }
+}
