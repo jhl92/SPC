33e520877bc43d7ba29b479ef8d2b6d52aa9e2e3
ZZZarkov <ZZZarkov@hotmail.com>
2014-03-19 11:13:16 +0100

    fhggfhgfhg

--- src/spc/SPC.java
+++ src/spc/SPC.java
@@ -14,8 +14,4 @@ public class SPC
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args)
-    {
-        // TODO code application logic here
-    }
 }
