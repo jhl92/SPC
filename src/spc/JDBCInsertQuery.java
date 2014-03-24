d5f7dde257cc705724d63071c925a0b915abd13e
Jonas <Jonas@192.168.1.105>
2014-03-24 11:54:14 +0100

    JDBC Added (implementation comes later)

--- src/spc/JDBCInsertQuery.java
+++ src/spc/JDBCInsertQuery.java
@@ -13,23 +13,23 @@ import java.sql.*;
 
 public class JDBCInsertQuery {
    // JDBC driver & database URL
-   static final String JDBC_DRIVER = "oracle.jdbc.driver.OracleDriver";  
-   static final String DB_URL = "jdbc:oracle:thin:@datdb.cphbusiness.dk:1521:dat";
+   static final String driver = "oracle.jdbc.driver.OracleDriver";  
+   static final String url = "jdbc:oracle:thin:@datdb.cphbusiness.dk:1521:dat";
 
    //  Database login
-   static final String USER = "";
-   static final String PASS = "";
+   static final String user = "";
+   static final String pass = "";
    
    public static void main(String[] args) {
    Connection conn = null;
    Statement stmt = null;
    try{
-      //Register JDBC driver
+      //Registrer JDBC driver
       Class.forName("oracle.jdbc.driver.OracleDriver");
 
       //Åben forbindelsen
       System.out.println("Connecting to a selected database...");
-      conn = DriverManager.getConnection(DB_URL, USER, PASS);
+      conn = DriverManager.getConnection(url, user, pass);
       System.out.println("Connected database successfully...");
       
       //Query
@@ -63,6 +63,6 @@ public class JDBCInsertQuery {
          se.printStackTrace();
       }
    }
-   System.out.println("Goodbye!");
+   System.out.println("Done.");
 }
 }
