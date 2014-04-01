0f14f560100185597a3fcc72a72ba3c1ed553ad4
Jonas <Jonas@192.168.1.105>
2014-04-01 09:45:16 +0200

    PrintWriter (DONE) - added returning guest Y/N to print and insert query

--- src/spc/JDBCInsertQuery.java
+++ src/spc/JDBCInsertQuery.java
@@ -8,114 +8,142 @@ package spc;
  *
  * @author Jonas
  */
-
 import java.sql.*;
 
-public class JDBCInsertQuery {
-   // JDBC driver & database URL
-   static final String driver = "oracle.jdbc.driver.OracleDriver";  
-   static final String url = "jdbc:oracle:thin:@datdb.cphbusiness.dk:1521:dat";
+public class JDBCInsertQuery
+{
+    // JDBC driver & database URL
 
-   //  Database login
-   static final String user = "SEM2_TEST_GR13";
-   static final String pass = "SEM2_TEST_GR13";
-   
-   public void JDBCInsertRoom(String guestID, String roomID, String empID, 
-            String dateFrom, String dateTo, String bookingPrice, 
+    static final String driver = "oracle.jdbc.driver.OracleDriver";
+    static final String url = "jdbc:oracle:thin:@datdb.cphbusiness.dk:1521:dat";
+    //  Database login
+    static final String user = "SEM2_TEST_GR13";
+    static final String pass = "SEM2_TEST_GR13";
+
+    public void JDBCInsertRoom(String guestID, String roomID, String empID,
+            String dateFrom, String dateTo, String bookingPrice,
             String guestFirstName, String guestLastName, String gCountry,
-            String gPhone, String gMail) {
-   Connection conn = null;
-   Statement stmt = null;
-   try{
-      //Registrer JDBC driver
-      Class.forName("oracle.jdbc.driver.OracleDriver");
+            String gPhone, String gMail, String returnGuest)
+    {
+        Connection conn = null;
+        Statement stmt = null;
+        try
+        {
+            //Registrer JDBC driver
+            Class.forName("oracle.jdbc.driver.OracleDriver");
 
-      //Åben forbindelsen
-      System.out.println("Connecting to a selected database...");
-      conn = DriverManager.getConnection(url, user, pass);
-      System.out.println("Connected database successfully...");
-      
-      //Query
-      System.out.println("Inserting records into the table...");
-      stmt = conn.createStatement();
-      
-      String sql = "INSERT INTO BOOKROOM VALUES ('"+guestID+"','" 
-              +roomID+"','"+empID+"','"+dateFrom+"','"+dateTo+"',"+bookingPrice+")";
-      stmt.executeUpdate(sql);
-      sql = "INSERT INTO GUEST VALUEs ('"+guestID+"','"+guestFirstName+"','"
-              +guestLastName+"','"+gCountry+"','"+gPhone+"','"+gMail+"')";
-      stmt.executeUpdate(sql);
-   
-      System.out.println("Inserted records into the table...");
+            //Åben forbindelsen
+            System.out.println("Connecting to a selected database...");
+            conn = DriverManager.getConnection(url, user, pass);
+            System.out.println("Connected database successfully...");
 
-   }catch(SQLException se){
-      //Håndterer JDBC relaterede fejl
-      se.printStackTrace();
-   }catch(Exception e){
-      //Håndterer Class relaterede fejl
-      e.printStackTrace();
-   }finally{
-      //Luk forbindelsen
-      try{
-         if(stmt!=null)
-            conn.close();
-      }catch(SQLException se){
-      }
-      try{
-         if(conn!=null)
-            conn.close();
-      }catch(SQLException se){
-         se.printStackTrace();
-      }
-   }
-   System.out.println("Done.");
-}
-   
-   public void JDBCInsertFacility(String guestID, String facType, String timeStart,
-           String timeEnd) {
-   Connection conn = null;
-   Statement stmt = null;
-   try{
-      //Registrer JDBC driver
-      Class.forName("oracle.jdbc.driver.OracleDriver");
+            //Query
+            System.out.println("Inserting records into the table...");
+            stmt = conn.createStatement();
 
-      //Åben forbindelsen
-      System.out.println("Connecting to a selected database...");
-      conn = DriverManager.getConnection(url, user, pass);
-      System.out.println("Connected database successfully...");
-      
-      //Query
-      System.out.println("Inserting records into the table...");
-      stmt = conn.createStatement();
-      
-      String sql = "INSERT INTO BOOKFACI VALUES ('"+guestID+"','" 
-              +facType+"','"+timeStart+"','"+timeEnd+"')";
+            String sql = "INSERT INTO BOOKROOM VALUES ('" + guestID + "','"
+                    + roomID + "','" + empID + "','" + dateFrom + "','" + dateTo 
+                    + "'," + bookingPrice + ")";
+            stmt.executeUpdate(sql);
+            sql = "INSERT INTO GUEST VALUEs ('" + guestID + "','" + guestFirstName 
+                    + "','" + guestLastName + "','" + gCountry + "','" 
+                    + gPhone + "','" + gMail + "','" + returnGuest + "')";
       stmt.executeUpdate(sql);
+
+            System.out.println("Inserted records into the table...");
+
+        } catch (SQLException se)
+        {
+            //Håndterer JDBC relaterede fejl
+            se.printStackTrace();
+        } catch (Exception e)
+        {
+            //Håndterer Class relaterede fejl
+            e.printStackTrace();
+        } finally
+        {
+            //Luk forbindelsen
+            try
+            {
+                if (stmt != null)
+                {
+                    conn.close();
+                }
+            } catch (SQLException se)
+            {
+            }
+            try
+            {
+                if (conn != null)
+                {
+                    conn.close();
+                }
+            } catch (SQLException se)
+            {
+                se.printStackTrace();
+            }
+        }
+        System.out.println("Done.");
+    }
+
+    public void JDBCInsertFacility(String guestID, String facType, String timeStart,
+            String timeEnd)
+    {
+        Connection conn = null;
+        Statement stmt = null;
+        try
+        {
+            //Registrer JDBC driver
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+
+            //Åben forbindelsen
+            System.out.println("Connecting to a selected database...");
+            conn = DriverManager.getConnection(url, user, pass);
+            System.out.println("Connected database successfully...");
+
+            //Query
+            System.out.println("Inserting records into the table...");
+            stmt = conn.createStatement();
+
+            String sql = "INSERT INTO BOOKFACI VALUES ('" + guestID + "','"
+                    + facType + "','" + timeStart + "','" + timeEnd + "')";
+            stmt.executeUpdate(sql);
 //      sql = "INSERT INTO";
 //      stmt.executeUpdate(sql);
 //   
-      System.out.println("Inserted records into the table...");
+            System.out.println("Inserted records into the table...");
 
-   }catch(SQLException se){
-      //Håndterer JDBC relaterede fejl
-      se.printStackTrace();
-   }catch(Exception e){
-      //Håndterer Class relaterede fejl
-      e.printStackTrace();
-   }finally{
-      //Luk forbindelsen
-      try{
-         if(stmt!=null)
-            conn.close();
-      }catch(SQLException se){
-      }
-      try{
-         if(conn!=null)
-            conn.close();
-      }catch(SQLException se){
-         se.printStackTrace();
-      }
-   }
-   System.out.println("Done.");
-}
+        } catch (SQLException se)
+        {
+            //Håndterer JDBC relaterede fejl
+            se.printStackTrace();
+        } catch (Exception e)
+        {
+            //Håndterer Class relaterede fejl
+            e.printStackTrace();
+        } finally
+        {
+            //Luk forbindelsen
+            try
+            {
+                if (stmt != null)
+                {
+                    conn.close();
+                }
+            } catch (SQLException se)
+            {
+            }
+            try
+            {
+                if (conn != null)
+                {
+                    conn.close();
+                }
+            } catch (SQLException se)
+            {
+                se.printStackTrace();
+            }
+        }
+        System.out.println("Done.");
+    }
 }
