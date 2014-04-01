d2e9e46e4c3656443f420dc3d4ea95002aaa4bb0
Jonas <Jonas@192.168.1.105>
2014-04-01 09:11:54 +0200

    PrintWriter (DONE) - minor improvements

--- src/spc/frameforsprint.java
+++ src/spc/frameforsprint.java
@@ -133,7 +133,7 @@ public class frameforsprint extends javax.swing.JFrame
 
         jLabel14.setText("E-mail:");
 
-        jButton1.setText("jButton1");
+        jButton1.setText("PrintWriter");
         jButton1.addActionListener(new java.awt.event.ActionListener()
         {
             public void actionPerformed(java.awt.event.ActionEvent evt)
@@ -347,12 +347,9 @@ public class frameforsprint extends javax.swing.JFrame
         // TODO add your handling code here:
     }//GEN-LAST:event_jTextField1ActionPerformed
 
-    /**
-     * @param args the command line arguments
-     */
+
     public static void main(String args[])
-    {
-        /* Set the Nimbus look and feel */
+    {   
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
         /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
          * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
@@ -381,8 +378,7 @@ public class frameforsprint extends javax.swing.JFrame
             java.util.logging.Logger.getLogger(frameforsprint.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
-
-        /* Create and display the form */
+   
         java.awt.EventQueue.invokeLater(new Runnable()
         {
             public void run()
