ebdec47c2e138988996574cf007a763a0f58af06
jhl92 <thomasnub@hotmail.com>
2014-04-23 14:35:51 +0200

    blck nkka

---
+++ src/spc/checkFacAvailabilityConstructor.java
@@ -0,0 +1,28 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Junheng Li
+ */
+public class checkFacAvailabilityConstructor
+{
+    private String guestID;
+
+    public String getGuestID()
+    {
+        return guestID;
+    }
+
+    public void setGuestID(String guestID)
+    {
+        this.guestID = guestID;
+    }
+
+    public checkFacAvailabilityConstructor(String guestID)
+    {
+        this.guestID = guestID;
+    }
+}
