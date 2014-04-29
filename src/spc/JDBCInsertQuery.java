9637c731386fcbf791f204c5a597315a17ac090b
Jalapeno1 <jons_91@hotmail.com>
2014-04-29 13:54:31 +0200

    SEM2_GR13 erstattet
    - Jonas

--- src/spc/JDBCInsertQuery.java
+++ src/spc/JDBCInsertQuery.java
@@ -14,8 +14,8 @@ public class JDBCInsertQuery
     static final String driver = "oracle.jdbc.driver.OracleDriver";
     static final String url = "jdbc:oracle:thin:@datdb.cphbusiness.dk:1521:dat";
     //  Database login
-    static final String user = "SEM2_TEST_GR13";
-    static final String pass = "SEM2_TEST_GR13";
+    static final String user = "SEM2_GR13";
+    static final String pass = "SEM2_GR13";
 
     public void JDBCInsertRoom(String guestID, String roomID, String empID,
             String dateFrom, String noOfNights, String dateTo, String roomAvailability,
--- src/spc/JDBCSelectQuery.java
+++ src/spc/JDBCSelectQuery.java
@@ -18,8 +18,8 @@ public class JDBCSelectQuery
     static final String driver = "oracle.jdbc.driver.OracleDriver";
     static final String url = "jdbc:oracle:thin:@datdb.cphbusiness.dk:1521:dat";
     //  Database login
-    static final String user = "SEM2_TEST_GR13";
-    static final String pass = "SEM2_TEST_GR13";
+    static final String user = "SEM2_GR13";
+    static final String pass = "SEM2_GR13";
     ResultSet rs;
     ArrayList<String> guestIDArray = new ArrayList<>();
 
